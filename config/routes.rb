31db4144025be908dc1c952d84a463c01688f30c
Rory McCune <rorym@mccune.org.uk>
2021-11-22 14:26:27 +0000

    home page and add deciduous logo to assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ # Set the static pages paths
+ get '/home', to: 'static_pages#home'
+ get '/about', to: 'static_pages#about'
+ # Set the root path
+ root 'static_pages#home'
 end
